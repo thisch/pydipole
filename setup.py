bd35e1f91035562b79e8278efd158a9010e44259
Thomas Hisch <thomas.hisch@tuwien.ac.at>
2015-12-22 08:55:58 +0100

    set version to 0.1.0
    Signed-off-by: Thomas Hisch <thomas.hisch@tuwien.ac.at>

--- setup.py
+++ setup.py
@@ -1,8 +1,18 @@
-from distutils.core import setup
+from setuptools import setup, find_packages
 from Cython.Build import cythonize
 import numpy
 
+__version__ = '0.1.0'
+
 setup(
+    author = 'Thomas Hisch',
+    author_email = 'thomas.hisch@tuwien.ac.at',
+    name='dipole',
+    packages=find_packages(),
     ext_modules=cythonize("dipole/field.pyx"),
-    include_dirs=[numpy.get_include()]
+    include_dirs=[numpy.get_include()],
+    platforms = 'Any',
+    requires = ['python (>=3.4.0)'],
+    version = __version__
+
 )
